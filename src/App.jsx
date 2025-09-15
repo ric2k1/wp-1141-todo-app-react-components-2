d92ce964b93d39873d0f542846f11537859dc517
ric2k1 <ric2k1@users.noreply.github.com>
2025-09-15 15:18:11 +0800

    Push state downwards

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import { useRef } from 'react'
+import { useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo'
 import TodoList from './components/TodoList'
 
 function App() {
-  const todoListRef = useRef()
+  const [todos, setTodos] = useState([
+    { id: 1, text: "todo 1", description: "This is the description for todo 1", completed: false, expanded: false },
+    { id: 2, text: "todo 2", description: "This is the description for todo 2", completed: false, expanded: false }
+  ])
 
-  const handleAddTodo = (newTodoItem) => {
-    if (todoListRef.current) {
-      todoListRef.current.addTodo(newTodoItem)
-    }
+  const addTodo = (newTodoItem) => {
+    setTodos([...todos, newTodoItem])
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ))
+  }
+
+  const toggleDescription = (id) => {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, expanded: !todo.expanded } : todo
+    ))
   }
 
   return (
     <div className="container">
       <h1 className="title">todo list</h1>
 
-      <AddTodo onAddTodo={handleAddTodo} />
+      <AddTodo onAddTodo={addTodo} />
 
-      <TodoList ref={todoListRef} />
+      <TodoList 
+        todos={todos}
+        onDelete={deleteTodo}
+        onToggle={toggleTodo}
+        onToggleDescription={toggleDescription}
+      />
     </div>
   )
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,51 +1,21 @@
-import { useState, forwardRef, useImperativeHandle } from 'react'
 import TodoItem from './TodoItem'
 
-const TodoList = forwardRef((props, ref) => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "todo 1", description: "This is the description for todo 1", completed: false, expanded: false },
-    { id: 2, text: "todo 2", description: "This is the description for todo 2", completed: false, expanded: false }
-  ])
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
-
-  const toggleTodo = (id) => {
-    setTodos(todos.map(todo => 
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ))
-  }
-
-  const toggleDescription = (id) => {
-    setTodos(todos.map(todo => 
-      todo.id === id ? { ...todo, expanded: !todo.expanded } : todo
-    ))
-  }
-
-  const addTodo = (newTodoItem) => {
-    setTodos([...todos, newTodoItem])
-  }
-
-  useImperativeHandle(ref, () => ({
-    addTodo
-  }))
-
+const TodoList = ({ todos, onDelete, onToggle, onToggleDescription }) => {
   return (
     <div className="todo-list">
       {todos.map((todo, index) => (
         <div key={todo.id}>
           <TodoItem
             todo={todo}
-            onToggle={toggleTodo}
-            onDelete={deleteTodo}
-            onToggleDescription={toggleDescription}
+            onToggle={onToggle}
+            onDelete={onDelete}
+            onToggleDescription={onToggleDescription}
           />
           {index < todos.length - 1 && <div className="separator" />}
         </div>
       ))}
     </div>
   )
-})
+}
 
 export default TodoList
